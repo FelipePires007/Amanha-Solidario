b255658d808893ecb1ecef458ab43ac85bf77b0c
AndreyJustino <andreyjustino2020@gmail.com>
2024-05-27 15:27:02 -0300

    Criando banco e primeira tabela

--- index.sql
+++ index.sql
@@ -1,1 +1,12 @@
--- Criação do arquivo
+CREATE DATABASE amanhaSolidario
+
+create table endereco(
+	cep int primary key not null,
+    rua varchar(255),
+    municipio varchar(255),
+    bairro varchar(255),
+    lati decimal(10,8),
+    longi decimal(11,8)
+);
+
+
