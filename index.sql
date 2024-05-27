2b803b99fa1a347348ae362b477fe66bca81f196
AndreyJustino <andreyjustino2020@gmail.com>
2024-05-27 15:27:52 -0300

    add insert para tabela endereco

--- index.sql
+++ index.sql
@@ -9,4 +9,9 @@ create table endereco(
     longi decimal(11,8)
 );
 
-
+insert into endereco (cep, rua, municipio, bairro, lati, longi)
+	values (12345678, 'Rua das Flores', 'São Paulo', 'Centro', -23.55052, -46.633308),
+		   (87654321, 'Avenida Brasil', 'Rio de Janeiro', 'Copacabana', -22.971089, -43.185997),
+		   (45678912, 'Rua 25 de Março', 'São Paulo', 'Brás', -23.541183, -46.629035),
+		   (98765432, 'Rua Oscar Freire', 'São Paulo', 'Jardins', -23.566501, -46.667672),
+           (65432198, 'Avenida Paulista', 'São Paulo', 'Bela Vista', -23.561268, -46.655503);
