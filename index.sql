48cd5382c2cd87ef5f45c0d2c333b4e6c12b3968
AndreyJustino <andreyjustino2020@gmail.com>
2024-05-27 15:30:52 -0300

    corrigindo erros: falta de ponto e virgula

--- index.sql
+++ index.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE amanhaSolidario
+CREATE DATABASE amanhaSolidario;
+
+use amanhaSolidario;
 
 create table endereco(
 	cep int primary key not null,
@@ -15,3 +17,5 @@ insert into endereco (cep, rua, municipio, bairro, lati, longi)
 		   (45678912, 'Rua 25 de Março', 'São Paulo', 'Brás', -23.541183, -46.629035),
 		   (98765432, 'Rua Oscar Freire', 'São Paulo', 'Jardins', -23.566501, -46.667672),
            (65432198, 'Avenida Paulista', 'São Paulo', 'Bela Vista', -23.561268, -46.655503);
+
+SELECT * FROM endereco;
