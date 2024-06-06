b5ba2c2b12309f11984b8e098bc099613a36498e
Junior Reis <gilmaikjunior@gmail.com>
2024-06-06 02:29:05 -0300

    add consultas e tirando erros de nos IDs

--- index.sql
+++ index.sql
@@ -3,7 +3,7 @@ CREATE DATABASE amanhaSolidario;
 USE amanhaSolidario;
 
 CREATE TABLE endereco(
-	cep INT PRIMARY KEY NOT NULL,
+    cep CHAR(8) PRIMARY KEY NOT NULL,
     rua VARCHAR(255),
     municipio VARCHAR(255),
     bairro VARCHAR(255),
@@ -15,96 +15,99 @@ CREATE TABLE doacao (
   id_doacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   quantidade INT NOT NULL,
   tipo VARCHAR(25) NOT NULL,
-  data_recebimento date NOT NULL
+  data_recebimento DATE NOT NULL
 );
 
 CREATE TABLE ong (
-  id_ong INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  cep INT,
-  projeto VARCHAR(255),
-  id_doacao INT,
-  nome VARCHAR(255),
-  gestor VARCHAR(255),
-  FOREIGN KEY (cep) REFERENCES endereco(cep),
-  FOREIGN KEY (id_doacao) REFERENCES doacao(id_doacao)
+    id_ong INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    cep CHAR(8),
+    projeto VARCHAR(255),
+    id_doacao INT,
+    nome VARCHAR(255),
+    gestor VARCHAR(255),
+    FOREIGN KEY (cep) REFERENCES endereco(cep),
+    FOREIGN KEY (id_doacao) REFERENCES doacao(id_doacao)
 );
 
 CREATE TABLE abrigo (
-  id_abrigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id_abrigo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   nome VARCHAR(255) NOT NULL,
   cep CHAR(8),
-  capacidade INTEGER NOT NULL,
-  gestor VARCHAR(255) NOT NULL
-
+  id_ong INT,
+  capacidade INT NOT NULL,
+  gestor VARCHAR(255) NOT NULL,
+  FOREIGN KEY (cep) REFERENCES endereco(cep),
+  FOREIGN KEY (id_ong) REFERENCES ong(id_ong)
 );
 
-
 CREATE TABLE vitima (
-  id_vitima INT AUTO_INCREMENT PRIMARY KEY,
-  cpf BIGINT,
-  nome VARCHAR(255) NOT NULL,
-  genero VARCHAR(255) NOT NULL,
-  idade INT,
-  id_abrigo INTEGER,
-  FOREIGN KEY (id_abrigo) REFERENCES abrigo(id_abrigo)
+    id_vitima INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    cpf CHAR(11),
+    nome VARCHAR(255) NOT NULL,
+    genero VARCHAR(255) NOT NULL,
+    idade INT,
+    id_abrigo INT,
+    FOREIGN KEY (id_abrigo) REFERENCES abrigo(id_abrigo)
 );
 
 CREATE TABLE voluntarios(
-  id_voluntarios INT AUTO_INCREMENT PRIMARY KEY,
+  id_voluntarios INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   numero_voluntarios INT,
   funcao VARCHAR(200),
   cpf CHAR(11),
-  id_ong int
+  id_ong INT,
+  FOREIGN KEY (id_ong) REFERENCES ong(id_ong)
 );
 
 INSERT INTO endereco (cep, rua, municipio, bairro, lati, longi)
-	values (12345678, 'Rua das Flores', 'São Paulo', 'Centro', -23.55052, -46.633308),
-		   (87654321, 'Avenida Brasil', 'Rio de Janeiro', 'Copacabana', -22.971089, -43.185997),
-		   (45678912, 'Rua 25 de Março', 'São Paulo', 'Brás', -23.541183, -46.629035),
-		   (98765432, 'Rua Oscar Freire', 'São Paulo', 'Jardins', -23.566501, -46.667672),
-           (65432198, 'Avenida Paulista', 'São Paulo', 'Bela Vista', -23.561268, -46.655503);
+VALUES 
+    ('12345678', 'Rua das Flores', 'São Paulo', 'Centro', -23.55052, -46.633308),
+    ('87654321', 'Avenida Brasil', 'Rio de Janeiro', 'Copacabana', -22.971089, -43.185997),
+    ('45678912', 'Rua 25 de Março', 'São Paulo', 'Brás', -23.541183, -46.629035),
+    ('98765432', 'Rua Oscar Freire', 'São Paulo', 'Jardins', -23.566501, -46.667672),
+    ('65432198', 'Avenida Paulista', 'São Paulo', 'Bela Vista', -23.561268, -46.655503);
 
-INSERT INTO doacao (id_doacao, quantidade, tipo, data_recebimento)
+INSERT INTO doacao (quantidade, tipo, data_recebimento)
 VALUES
-    (1, 10, 'Alimento', '2024-05-26'),
-    (2, 5, 'Roupa', '2024-05-16'),
-    (3, 20, 'Alimento', '2024-05-01'),
-    (4, 15, 'Alimento', '2024-04-24'),
-    (5, 8, 'Roupa', '2024-04-11');
+    (10, 'Alimento', '2024-05-26'),
+    (25, 'Roupa', '2024-05-16'),
+    (20, 'Alimento', '2024-05-01'),
+    (15, 'Alimento', '2024-04-24'),
+    (40, 'Roupa', '2024-04-11');
 
-INSERT INTO  ong (id_ong, cep, projeto, id_doacao, nome, gestor)
-values
-  (1, 12345678, 'Projeto Alimentar', 1, 'ONG Alegria', 'João Silva'),
-  (2, 87654321, 'Projeto Vestir', 2, 'ONG Esperança', 'Maria Souza'),
-  (3, 45678912, 'Projeto Nutrir', 3, 'ONG Solidariedade', 'Carlos Pereira'),
-  (4, 98765432, 'Projeto Educação', 4, 'ONG Futuro', 'Ana Lima'),
-  (5, 65432198, 'Projeto Incluir', 5, 'ONG Inclusão', 'Paulo Santos');
+INSERT INTO ong (cep, projeto, id_doacao, nome, gestor)
+VALUES
+    ('12345678', 'Projeto Alimentar', 1, 'ONG Alegria', 'João Silva'),
+    ('87654321', 'Projeto Vestir', 2, 'ONG Esperança', 'Maria Souza'),
+    ('45678912', 'Projeto Nutrir', 3, 'ONG Solidariedade', 'Carlos Pereira'),
+    ('98765432', 'Projeto Educação', 4, 'ONG Futuro', 'Ana Lima'),
+    ('65432198', 'Projeto Incluir', 5, 'ONG Inclusão', 'Paulo Santos');
 
+INSERT INTO abrigo (nome, cep, id_ong, capacidade, gestor)
+VALUES
+    ('Abrigo Soledade', '12345678', 1, 50, 'Gil Maik'),
+    ('Abrigo Bosque da Paz', '87654321', 2, 70, 'Andrey Justino'),
+    ('Abrigo Viladouro', '87654321', 3, 75, 'Kaique Alexandre'),
+    ('Abrigo Bom Viver', '12345678', 4, 60, 'Felipe Pires'),
+    ('Abrigo Amanhã Melhor', '65432198', 5, 50, 'Gabriel Santos');
 
-INSERT INTO abrigo (id_abrigo, nome, cep, capacidade, gestor)
-values
-    (1, 'Abrigo Soledade', '12344321', 50, 'Gil Maik'),
-    (2, 'Abrigo Bosque da Paz', '43211234', 70, 'Andrey Justino'),
-    (3, 'Abrigo Viladouro', '87654321', 75, 'Kaique Alexandre'),
-    (4, 'Abrigo Bom Viver', '12345678', 60, 'Felipe Pires'),
-    (5, 'Abrigo Amanhã Melhor', '81726354', 50, 'Gabriel Santos');
+INSERT INTO vitima (cpf, nome, genero, idade, id_abrigo)
+VALUES
+    ('12345678901', 'Maria Souza', 'Feminino', 25, 1),
+    ('98765432109', 'Joaquim Ferreira', 'Masculino', 30, 2),
+    ('45678901234', 'Ana Souza', 'Feminino', 28, 3),
+    ('78901234567', 'Carlos Almeida', 'Masculino', 35, 4),
+    ('23456789012', 'Carla Costa', 'Feminino', 22, 5);
 
+INSERT INTO voluntarios (numero_voluntarios, funcao, cpf, id_ong)
+VALUES 
+    (40, 'Secretaria da Saude', '12345608921', 1),
+    (38, 'Correios', '12345670891', 2),
+    (55, 'Corpo de Bombeiros', '1234506783', 3),
+    (32, 'Resgate de Animais', '1234056781', 4),
+    (25, 'Distribuir Comida', '12304567821', 5);
 
-INSERT INTO vitima(cpf, nome, genero, idade, id_abrigo)
-values
-    (12345678901, 'Maria Souza', 'Feminino', 25, 1),
-    (98765432109, 'Joaquim Ferreira', 'Masculino', 30, 1),
-    (45678901234, 'Ana Souza', 'Feminino', 28, 2),
-    (78901234567, 'Carlos Almeida', 'Masculino', 35, 3),
-    (23456789012, 'Carla Costa', 'Feminino', 22, 1);
-
-INSERT INTO voluntarios(numero_voluntarios, funcao, cpf)
-    values 
-    (1, 'secretaria da saude', 12345608921),
-    (2, 'correios', 12345670891),
-    (3, 'corpo de bombeiros', 1234506783),
-    (4, 'resgate de animais', 1234056781),
-    (5, 'distribuir comida', 12304567821);
+-- Select all data from the tables
 
 SELECT * FROM endereco;
 
@@ -118,21 +121,25 @@ SELECT * FROM vitima;
 
 SELECT * FROM voluntarios;
 
-SELECT tipo, SUM(quantidade) AS total_quantidade
+-- Gil Maik
+SELECT tipo, SUM(quantidade) AS quantidade_total
 FROM doacao
 GROUP BY tipo;
+
+ 
+SELECT
+  abrigo.nome AS Nome,
+  abrigo.capacidade AS Capacidade
+FROM abrigo
+ORDER BY abrigo.capacidade;
+
+SELECT
+  abrigo.nome AS Nome,
+  (abrigo.capacidade - (SELECT SUM(vitima.idade) FROM vitima WHERE vitima.id_abrigo = abrigo.id_abrigo)) AS Vagas
+FROM abrigo
+LEFT JOIN vitima ON vitima.id_abrigo = abrigo.id_abrigo
+GROUP BY abrigo.id_abrigo, abrigo.nome, abrigo.capacidade;
 
 SELECT genero, COUNT(id_vitima) AS quantidade_vitimas
 FROM vitima
 GROUP BY genero;
-
-SELECT 
-    abrigo.nome AS nome_abrigo,
-    COUNT(vitima.id_vitima) AS quantidade_vitimas,
-    abrigo.capacidade AS capacidade_abrigo
-FROM 
-    abrigo
-LEFT JOIN 
-    vitima ON abrigo.id_abrigo = vitima.id_abrigo
-GROUP BY 
-    abrigo.nome, abrigo.capacidade;
